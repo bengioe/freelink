c97a57cceddc540ef8720ca77398e99314250a04
Leo Long <leo.teng.long@gmail.com>
2016-02-09 15:33:01 -0500

    Complete extract stage

--- process/wikilink.py
+++ process/wikilink.py
@@ -11,22 +11,27 @@ from gzip import GzipFile
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from newspaper import Article
-from nltk import word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import wordpunct_tokenize
+import os
 import re
 import sys
+import ftfy
 import json
+import string
+import jsonrpc
 
 
 ###############################################
 # function to extract data from a single file #
 ###############################################
-def extract_data(fname, loadp, savep):
+def extract_data(fname, server, loadp, savep):
     # load file using thrift frameword
     stream = GzipFile(loadp + fname)
     protocol = TBinaryProtocol.TBinaryProtocol(TTransport.TBufferedTransport(stream))
     data = {'data': []}
 
-    for i in range(0, 30):
+    while True:
         # read next data entry
         page = WikiLinkItem()
         try:
@@ -49,32 +54,58 @@ def extract_data(fname, loadp, savep):
             continue
 
         # extract all unique entities among mentions
-        entities = build_dict(extract_entities(page.mentions))
-        # ignore pages with single entity
+        entities = extract_entities(page.mentions)
+        # discard pages with single entity
         if len(entities) < 2:
-            print '\t\t ###### Single entity found (ignore) ###### \n'
+            print '\t\t ###### Single entity found (discard) ###### \n'
             continue
 
         # mark dom string
         html = mark_dom(page.content.dom, entities)
-        # skip pages that failed to be marked
+        # discard pages that failed to be marked
         if html == None:
             print '\t\t ###### Alignment failed (discard) ###### \n'
             continue
 
+        # parse marked html
         news = Article(page.url, language = 'en')
-        news.set_html(page.content.dom)
-        f = open(savep + '{0}.html'.format(i), 'w')
-        f.write(news.html.encode('ascii', 'ignore'))
-        f.close()
         news.set_html(html)
-        f = open(savep + '{0}_marked.html'.format(i), 'w')
-        f.write(news.html.encode('ascii', 'ignore'))
-        f.close()
+        # discard pages that cannot be parsed
+        try:
+            news.parse()
+        except:
+            print '\t\t ###### Parsing failed (discard) ###### \n'
+            continue
 
-        g = open(savep + '{0}.json'.format(i), 'w')
-        json.dump(entities, g, indent = 4)
-        g.close()
+        # tokenize text
+        text = None
+        try:
+            text = ftfy.fix_text(news.text)
+            text = text.encode('ascii', 'ignore')
+            text = seperate_punc(word_tokenize(text))
+            '''
+            # Unreliable
+            text = ' '.join(word_tokenize(text))
+            text = ' '.join(wordpunct_tokenize(text))
+            '''
+            '''
+            # Un-comment to tokenize with CoreNLP Server
+            payload = json.loads(server.parse(text))
+            text = recover_text(payload)
+            '''
+        except:
+            print '\t\t ###### Tokenization failed (discard) ###### \n'
+            continue
+
+        # save processed data
+        print '\t [DATA SAVED] \n'
+        data['data'].append({'text': text, 'dict': entities})
+
+    # Save data to json file
+    print '****** {0}.json saved ******\n'.format(fname[:3])
+    f = open(savepath + '{0}.json'.format(fname[:3]), 'w')
+    json.dump(data, f, indent = 4)
+    f.close()
 
 
 #######################################
@@ -92,23 +123,15 @@ def extract_entities(mentions):
         result.append(m)
         seen.add(m.freebase_id)
 
-    # return type: mention-object list
-    return result
-
-
-#######################################
-# function to build entity dictionary #
-#######################################
-def build_dict(entities):
     dictionary = {}
 
-    for i in range(0, len(entities)):
+    for i in range(0, len(result)):
         mark = 'marker_{0}'.format(i)
-        dictionary[mark] = {'anchor_text': entities[i].anchor_text,
-                            'freebase_id': entities[i].freebase_id,
-                            'wiki_url': entities[i].wiki_url}
+        dictionary[mark] = {'anchor_text': result[i].anchor_text,
+                            'freebase_id': result[i].freebase_id,
+                            'wiki_url':    result[i].wiki_url}
 
-    # return type: entity dictionary (key: marker token)
+    # return type: entity dictionary
     return dictionary
 
 ###############################################
@@ -119,8 +142,7 @@ def mark_dom(dom, entities):
     currhtml = ''
 
     for key, value in entities.iteritems():
-        iterator = None
-        match = None
+        iterator, match = None, None
         try:
             iterator = re.finditer(value['wiki_url'], prevhtml)
             match = [m for m in iterator]
@@ -128,20 +150,23 @@ def mark_dom(dom, entities):
             # return type: 'None' to signal failure
             return None
 
-        index = find_tagidx(prevhtml, match)
-        for i in range(0, len(index)):
-            # isolate entity mention
-            start1, end1, start2, end2 = -1, -1, -1, -1
-            if len(index) == 1:
-                start1, end1, start2, end2 = 0, index[i][0], index[i][1], len(prevhtml)
-            elif i == 0:
-                start1, end1, start2, end2 = 0, index[i][0], index[i][1], index[i + 1][0]
-            elif i == len(index) - 1:
-                start1, end1, start2, end2 = index[i - 1][1], index[i][0], index[i][1], len(prevhtml)
+        if len(match) == 0:
+            continue
+
+        tagidx = find_tagidx(prevhtml, match)
+
+        for i in range(0, len(tagidx)):
+            # find index to isolate entity mention
+            start, end = -1, -1
+            if i == 0:
+                start, end = 0, tagidx[i]['start']
             else:
-                start1, end1, start2, end2 = index[i - 1][1], index[i][0], index[i][1], index[i + 1][0]
-            # mark entity mention
-            currhtml += prevhtml[start1:end1] + key + prevhtml[start2:end2]
+                start, end = tagidx[i - 1]['end'], tagidx[i]['start']
+            # substitude entity mention with its marker
+            currhtml += prevhtml[start:end] + ' {0} '.format(key)
+            # concatinate remaining file
+            if i == len(tagidx) - 1:
+                currhtml += prevhtml[tagidx[i]['end']:]
 
         prevhtml = currhtml
         currhtml = ''
@@ -149,26 +174,69 @@ def mark_dom(dom, entities):
     # return type: marked string
     return prevhtml
 
-
-# function to find the index of html tags
+###########################################
+# function to find the index of html tags #
+###########################################
 def find_tagidx(html, match):
     index = []
 
     for m in match:
-        sidx = m.start()
-        eidx = m.end()
+        sidx, eidx = m.start(), m.end()
         while html[sidx:sidx + 2] != '<a':
             sidx -= 1
         while html[eidx - 2:eidx] != 'a>':
             eidx += 1
-        index.append((sidx, eidx))
+        index.append({'start': sidx, 'end': eidx})
 
+    # return type: tag index pair list
     return index
 
 
-# main function
-if __name__ == '__main__':
-    loadpath = '/scratch/data/wikilinks/raw/'
-    savepath = '/scratch/data/wikilinks/'
+#############################################
+# function to handle punctuation at the end #
+#############################################
+def seperate_punc(tokens):
+    words = []
+    puncs = set(',.:;!?')
 
-    extract_data('001.gz', loadpath, savepath)
+    for t in tokens:
+        end = len(t) - 1
+        if (t[end] not in puncs) or (t in puncs):
+            words.append(t)
+        else:
+            words.append(t[:end])
+            words.append(t[end])
+
+    # return type: processed string
+    return ' '.join(words)
+
+
+##############################################
+# function to recover text from json payload #
+##############################################
+def recover_text(payload):
+    words = []
+
+    for s in payload['sentences']:
+        for w in s['words']:
+            words.append(w[0])
+
+    return ' '.join(words)
+
+
+#################
+# main function #
+#################
+if __name__ == '__main__':
+    loadpath = '/scratch/data/wikilink/raw/'
+    savepath = '/scratch/data/wikilink/ext/'
+
+    if sys.argv[1] == 'extract':
+        fnames = os.listdir(loadpath)
+        server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(),
+                                     jsonrpc.TransportTcpIp(addr = ('127.0.0.1', 8080)))
+
+        fnames.sort()
+        for i in range(0, 10):
+            print '****** processing {0} ******\n'.format(fnames[i])
+            extract_data(fnames[i], server, loadpath, savepath)
